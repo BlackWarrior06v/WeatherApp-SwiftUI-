9f657e565edd758f8b86985aadded15d3558d6ea
BlackWarrior06v <curipch2@gmail.com>
2024-12-15 21:54:07 -0600

    Refactoring

---
+++ WeatherApp/WeatherButton.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherButton.swift
+//  WeatherApp
+//
+//  Created by Curi on 15/12/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    
+    var body: some View{
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight:  .bold, design: .default))
+            .cornerRadius(10)
+    }
+    
+}
